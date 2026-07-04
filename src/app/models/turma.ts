3e476d834ffe1d4e23a094e8a5f91827ca5ce38e
Rui Leite <ruipimentelleite@gmail.com>
2026-07-04 09:00:22 -0300

    Cria model `Turma`.
    - Comando:
      ng generate class models/turma

---
+++ src/app/models/turma.ts
@@ -0,0 +1,18 @@
+import { Aluno } from "./aluno";
+import { Disciplina } from "./disciplina";
+
+export class Turma {
+
+  constructor(
+    public readonly disciplina: Disciplina,
+    public readonly ano: number,
+    public readonly periodo: number,
+    public alunos: Aluno,
+  ) {
+  }
+
+  public get nome(): string {
+    return `${this.disciplina.codigo}-${this.ano}/${this.periodo}`;
+  }
+
+}
